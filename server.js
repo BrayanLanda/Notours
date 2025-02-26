02cd7eefe91a1b9fcfa537ab6dc843e36fdb7fc5
Brayan Acosta <landakkpp@gmail.com>
2025-02-25 22:55:51 -0500

    refactor code

--- server.js
+++ server.js
@@ -7,7 +7,7 @@ dotenv.config({path: './config.env'});
 const DB = process.env.DATABASE.replace('<PASSWORD>', process.env.DATABASE_PASSWORD ); 
 
 mongoose.connect(DB)
-    .then(() => console.log('DATABASE connection successfull..'));
+    .then(() => console.log('DATABASE connection successfully..'));
 
 const port = process.env.PORT || 3000;
 
